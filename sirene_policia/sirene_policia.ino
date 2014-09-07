2b70ac1e41c683997135a6a5e2e473977318eacc
Ezequiel <ezequiel.ifsp@gmail.com>
2014-09-07 14:18:41 -0300

    outros

---
+++ sirene_policia/sirene_policia.ino
@@ -0,0 +1,24 @@
+
+//Programa : Som no Arduino - Sirene
+//Autor : Arduino e Cia
+
+#define tempo 10
+int frequencia = 0;
+int Pinofalante = 13;
+
+void setup()
+{
+  pinMode(Pinofalante,OUTPUT); //Pino do buzzer
+}
+void loop()
+{
+  for (frequencia = 150; frequencia < 1800; frequencia += 1) {
+    tone(Pinofalante, frequencia, tempo); 
+  delay(1);
+  }
+  for (frequencia = 1800; frequencia > 150; frequencia -= 1) {
+    tone(Pinofalante, frequencia, tempo); 
+  delay(1);
+  }
+}
+
